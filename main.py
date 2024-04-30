f67c6938f8a8d910bdbcdf1c2baf4efcad398f5d
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-04-30 11:15:00 +0100

    Improved input

--- main.py
+++ main.py
@@ -1,18 +1,38 @@
 from PointOptimizationMethods import PointOptimizationMethods
 import sympy as sp
 
-
 if __name__ == '__main__':
+    # Define the symbolic expression for Newton's method
     x = sp.symbols('x')
-    f = x**4 / 20 + x / 4 + 1  # Symbolic function for Newton's method
+    f = x ** 4 / 20 + x / 4 + 1
+
+
+    # Define the function for gradient descent
     def fun(x):
-        return x**4 / 20 + x / 4 + 1  # Regular Python function for gradient descent
+        return x ** 4 / 20 + x / 4 + 1
+
 
     # Newton's optimization method
-    print(PointOptimizationMethods.newtons_method(f, x_k=3))
+    x_optimal_newton, best_function_value_newton, iterations_newton = PointOptimizationMethods.newtons_method(f, x_k=3)
+    print("Newton's Method:")
+    print("Optimal value (x):", x_optimal_newton)
+    print("Best function value:", best_function_value_newton)
+    print("Iterations:", iterations_newton)
+    print()
 
     # Gradient optimization method
-    print(PointOptimizationMethods.gradient_method(fun, uk=3))
+    x_optimal_gradient, best_function_value_gradient, iterations_gradient = PointOptimizationMethods.gradient_method(
+        fun, uk=3)
+    print("Gradient Descent Method:")
+    print("Optimal value (x):", x_optimal_gradient)
+    print("Best function value:", best_function_value_gradient)
+    print("Iterations:", iterations_gradient)
+    print()
 
     # Random search optimization method
-    print(PointOptimizationMethods.random_search(f, x_k=3, shrink_step=True))
+    x_optimal_random, best_function_value_random, iterations_random = PointOptimizationMethods.random_search(f, x_k=3,
+                                                                                                             shrink_step=True)
+    print("Random Search Method:")
+    print("Optimal value (x):", x_optimal_random)
+    print("Best function value:", best_function_value_random)
+    print("Iterations:", iterations_random)
