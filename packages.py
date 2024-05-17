d4fa0ec2340426cfdbbe537b0edf7c66644a64ed
Yehor Boiar <yb33@st-andrews.ac.uk>
2024-05-17 11:05:47 +0100

    Added new file that install all packages for us

---
+++ packages.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+
+# List of libraries to install
+libraries = [
+    "matplotlib",
+    "pandas",
+    "numpy",
+    "scipy",
+    "sympy"  # Fixed the typo: 'symoy' to 'sympy'
+]
+
+# Function to install a package using pip
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Install each library
+for library in libraries:
+    install(library)
+
+print("All libraries installed successfully!")
